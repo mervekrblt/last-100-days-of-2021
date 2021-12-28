7785b2e5b4f6a66a91f80224a447476b9e502140
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-12-29 01:21:18 +0300

    Finally I add styling :)

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 const didnothing = document.querySelectorAll(".free")
-console.log(didnothing)
+
+const info = document.querySelector(".info")
+
+info.append(`I havent done anything for ${didnothing.length} days in the last 100 days of 2021`)
